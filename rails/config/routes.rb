0e9070e4b0a54e111d8411178904435cc318562c
Naoki Takashima <39912547+takatea@users.noreply.github.com>
2023-08-22 09:02:43 +0000

    chore: bundle exec rubocop -a

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount KomachiHeartbeat::Engine => "/ops"
+  mount KomachiHeartbeat::Engine => '/ops'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
