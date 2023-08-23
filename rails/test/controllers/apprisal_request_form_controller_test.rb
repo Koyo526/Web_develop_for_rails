19aa5e95284fe58a32073bbab5e1101156fdd4be
t4jet <53136890+t4jet@users.noreply.github.com>
2023-08-23 10:07:30 +0000

    テストの削除

--- rails/test/controllers/apprisal_request_form_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-class ApprisalRequestFormControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-    get apprisal_request_form_index_url
-    assert_response :success
-  end
-end
--- rails/test/controllers/assessment_requests_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-class AssessmentRequestsControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-    get assessment_requests_index_url
-    assert_response :success
-  end
-end
