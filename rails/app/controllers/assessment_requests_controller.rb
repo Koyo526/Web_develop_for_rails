d5367f0aba8a5fa3194eb48303001b64f75031e5
t4jet <53136890+t4jet@users.noreply.github.com>
2023-08-23 05:48:18 +0000

    bunele exec rubocop -a

--- rails/app/controllers/assessment_requests_controller.rb
+++ rails/app/controllers/assessment_requests_controller.rb
@@ -1,4 +1,5 @@
+# frozen_string_literal: true
+
 class AssessmentRequestsController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- rails/test/controllers/assessment_requests_controller_test.rb
+++ rails/test/controllers/assessment_requests_controller_test.rb
@@ -1,7 +1,8 @@
-require "test_helper"
+# frozen_string_literal: true
 
+require 'test_helper'
 class AssessmentRequestsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get assessment_requests_index_url
     assert_response :success
   end
