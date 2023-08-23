017371c3a1ef9d4dec04faa2a8d88591cf7b4c9e
t4jet <53136890+t4jet@users.noreply.github.com>
2023-08-23 01:42:52 +0000

    bundle exec rubocop -a

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   get 'apprisal_request_form', to: 'apprisal_request_form#index'
-  mount KomachiHeartbeat::Engine => "/ops"
+  mount KomachiHeartbeat::Engine => '/ops'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- rails/test/controllers/apprisal_request_form_controller_test.rb
+++ rails/test/controllers/apprisal_request_form_controller_test.rb
@@ -1,7 +1,8 @@
-require "test_helper"
+# frozen_string_literal: true
 
+require 'test_helper'
 class ApprisalRequestFormControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get apprisal_request_form_index_url
     assert_response :success
   end
