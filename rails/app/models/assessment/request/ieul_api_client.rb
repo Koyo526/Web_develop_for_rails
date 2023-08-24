2019dbb6172ed4729563faceb349425cb5bc088e
中田愛刀 <71203048+Aito-Nakata@users.noreply.github.com>
2023-08-24 05:49:05 +0000

    initialize よせました
    リファクタリングしたよ

--- rails/app/models/assessment/request/ieul_api_client.rb
+++ rails/app/models/assessment/request/ieul_api_client.rb
@@ -6,15 +6,19 @@ require 'json'
 class Assessment::Request::IeulApiClient
   API_ENDPOINT = 'https://miniul-api.herokuapp.com/affiliate/v2'
 
-  def post(params)
-    uri = URI.parse("#{API_ENDPOINT}/conversions")
-    http = Net::HTTP.new(uri.host, uri.port).tap do | h |
+  attr_reader :net_http
+
+  def initialize
+    uri = URI.parse(API_ENDPOINT)
+    @net_http = Net::HTTP.new(uri.host, uri.port).tap do | h |
       h.use_ssl = true
       h.verify_mode = OpenSSL::SSL::VERIFY_NONE
     end
+  end
 
-    http.start do
-      req = Net::HTTP::Post.new(uri.path)
+  def post(params)
+    net_http.start do |http|
+      req = Net::HTTP::Post.new("/affiliate/v2/conversions")
       req.set_form_data(params)
       http.request(req)
     end
