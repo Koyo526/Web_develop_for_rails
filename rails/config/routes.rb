9bf632f4df411ef5798790fa0140b50f30ff2a4f
t4jet <53136890+t4jet@users.noreply.github.com>
2023-08-24 04:12:25 +0000

    add: resources :thanks, only: %i[index]

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'thanks/index'
+  resources :thanks, only: %i[index]
   resources :assessment_requests, only: %i[index create]
   mount KomachiHeartbeat::Engine => '/ops'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
