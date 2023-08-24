3b60f30825cecf28eebfc31f3e346507735a7a0b
t4jet <53136890+t4jet@users.noreply.github.com>
2023-08-24 08:09:11 +0000

    bundle exec rubocop -a

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -21,9 +21,9 @@ module Rail
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators do |g|
-      g.test_framework  :rspec, :fixture => false
+      g.test_framework :rspec, fixture: false
       g.view_specs      false
       g.helper_specs    false
-  end
+    end
   end
 end
