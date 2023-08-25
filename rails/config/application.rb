ea9ad95549d822b68909c9f698064768cc22907a
中田愛刀 <71203048+Aito-Nakata@users.noreply.github.com>
2023-08-25 08:35:09 +0000

    [#9]レビューの解消

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -20,5 +20,11 @@ module Rail
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
   end
 end
