f5e99dc6ca146e37850dcef5dc6dde9a6c0e258a
t4jet <53136890+t4jet@users.noreply.github.com>
2023-08-28 09:25:24 +0000

    bundle exec rubocop -A

--- rails/app/controllers/companys_controller.rb
+++ rails/app/controllers/companys_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class CompanysController < ApplicationController
-  def index;
+  def index
     return unless params[:ieul_office_id]
 
     @office = Office.find(params[:ieul_office_id])
     @ieul_id = Office.find(params[:ieul_office_id]).ieul_id
-    @company_name =  Company.find(@ieul_id).name
+    @company_name = Company.find(@ieul_id).name
     @reviews = @office.reviews.order(release_date: :desc).limit(5)
 
     total_satisfaction = @reviews.sum(:contract_price_satisfaction)
