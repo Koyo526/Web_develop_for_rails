f7b1f9ef6a744ab4108150cb5fe152f6c47839c5
中田愛刀 <71203048+Aito-Nakata@users.noreply.github.com>
2023-08-28 02:09:23 +0000

    [#9]conflictの解消

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -20,7 +20,6 @@ module Rail
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
