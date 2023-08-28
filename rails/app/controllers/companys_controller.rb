6fba987d34256de644328663264b12f771be48b8
t4jet <53136890+t4jet@users.noreply.github.com>
2023-08-28 09:21:10 +0000

    口コミの平均評価を表示

--- rails/app/controllers/companys_controller.rb
+++ rails/app/controllers/companys_controller.rb
@@ -8,5 +8,8 @@ class CompanysController < ApplicationController
     @ieul_id = Office.find(params[:ieul_office_id]).ieul_id
     @company_name =  Company.find(@ieul_id).name
     @reviews = @office.reviews.order(release_date: :desc).limit(5)
+
+    total_satisfaction = @reviews.sum(:contract_price_satisfaction)
+    @average_satisfaction = total_satisfaction / @reviews.length.to_f
   end
 end
