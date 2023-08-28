a07ace57075fae629d646f3a4cdefe27844a869d
t4jet <53136890+t4jet@users.noreply.github.com>
2023-08-28 09:34:08 +0000

    リダイレクト指定

--- rails/app/controllers/reviews_controller.rb
+++ rails/app/controllers/reviews_controller.rb
@@ -2,12 +2,14 @@
 
 class ReviewsController < ApplicationController
   def index
-    return unless params[:id]
-
-    @review = Review.find(params[:id])
-    @company_id = Review.find(params[:id]).ieul_id
-    @company_name = Company.find(@company_id).name
-    @office_id = Review.find(params[:id]).ieul_office_id
-    @office = Office.find(@office_id)
+    if params[:id]
+        @review = Review.find(params[:id])
+        @company_id = Review.find(params[:id]).ieul_id
+        @company_name = Company.find(@company_id).name
+        @office_id = Review.find(params[:id]).ieul_office_id
+        @office = Office.find(@office_id)
+    else 
+        render '/'
+    end
   end
 end
