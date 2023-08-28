9f7b11e563c9bc2b0c8e3f2a55743c7d38c32af9
t4jet <53136890+t4jet@users.noreply.github.com>
2023-08-28 07:11:44 +0000

    企業ページ作成

--- rails/app/controllers/companys_controller.rb
+++ rails/app/controllers/companys_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class CompanysController < ApplicationController
-  def index; end
+  def index;
+    return unless params[:ieul_office_id]
+
+    @office = Office.find(params[:ieul_office_id])
+    @ieul_id = Office.find(params[:ieul_office_id]).ieul_id
+    @company_name =  Company.find(@ieul_id).name
+    @reviews = @office.reviews.order(release_date: :desc).limit(5)
+  end
 end
