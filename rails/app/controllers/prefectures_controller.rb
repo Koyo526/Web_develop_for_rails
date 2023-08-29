bf9d7086c972fbe7ccc725052b4a59ffc6066b74
t4jet <53136890+t4jet@users.noreply.github.com>
2023-08-29 05:38:39 +0000

    bundle exec rubocop -A

--- rails/app/controllers/prefectures_controller.rb
+++ rails/app/controllers/prefectures_controller.rb
@@ -5,6 +5,6 @@ class PrefecturesController < ApplicationController
     @offices = Office.all
     # https://musical-space-rotary-phone-xpw57g5999qfppw6-3000.app.github.dev/prefectures/1/cities/1
     @city = City.find(params[:city_id])
-    @cities_without_target_city = @city.prefecture.cities.reject { |city| city == @city.id }
+    @cities_without_target_city = @city.prefecture.cities.reject { |city| city == @city }
   end
 end
