9d439da6e81d1744778d9eba0014bcbbdfece7c3
KoMaTop10 <74088338+KoMaTop10@users.noreply.github.com>
2023-08-29 03:29:38 +0000

    assessable_areaのマイグレーションファイルを追加

---
+++ rails/db/migrate/20230828014451_create_assessable_areas.rb
@@ -0,0 +1,9 @@
+class CreateAssessableAreas < ActiveRecord::Migration[7.0]
+  def change
+    create_table :assessable_areas do |t| #査定可能エリア中間テーブル
+      t.references :office, null: false, foreign_key: true, comment: "企業ID"
+      t.references :city, null: false, foreign_key: true, comment: "市区町村ID"
+    end
+    # add_index :assessable_areas,[:city_id,:office_id],unique: true
+  end
+end
