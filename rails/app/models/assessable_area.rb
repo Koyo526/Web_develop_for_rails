35bb716ed1c788429cb72e30d15d9f335ee0ab95
KoMaTop10 <74088338+KoMaTop10@users.noreply.github.com>
2023-08-29 03:30:20 +0000

    city,office,assessable_areaのリレーションを追記

---
+++ rails/app/models/assessable_area.rb
@@ -0,0 +1,4 @@
+class AssessableArea < ApplicationRecord
+    belongs_to :office
+    belongs_to :city
+end
--- rails/app/models/city.rb
+++ rails/app/models/city.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class City < ApplicationRecord
+  belongs_to :prefecture
   has_many :offices, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  belongs_to :prefecture
+  has_many :assessable_areas, foreign_key: :city_id
+  has_many :assessable_offices ,through: :assessable_areas ,source: :office
 end
