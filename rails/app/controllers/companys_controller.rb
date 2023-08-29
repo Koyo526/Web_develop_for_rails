8f98df3a67eb30124dfc800ee170c3a5502abf20
Naoki Takashima <39912547+takatea@users.noreply.github.com>
2023-08-29 09:57:05 +0000

    feat: コントローラーからdataを渡してレーダーチャートを表示できるようにしたよ

--- rails/app/controllers/companys_controller.rb
+++ rails/app/controllers/companys_controller.rb
@@ -6,7 +6,8 @@ class CompanysController < ApplicationController
 
     @office = Office.find(params[:ieul_office_id])
     @ieul_id = Office.find(params[:ieul_office_id]).ieul_id
-    @company_name = Company.find(@ieul_id).name
+    @company = Company.find(@ieul_id)
+    @company_name =  @company.name
     @reviews = @office.reviews.order(release_date: :desc).limit(5)
 
     total_satisfaction = @reviews.sum(:contract_price_satisfaction)
@@ -17,5 +18,16 @@ class CompanysController < ApplicationController
 
     total_sales_speed_satisfaction = @reviews.sum(:sales_speed_satisfaction)
     @average_sales_speed_satisfaction = total_sales_speed_satisfaction / @reviews.length.to_f
+
+    # data: [<%= @average_satisfaction%>, <%= @average_contract_price_satisfaction %>, <%= @average_sales_speed_satisfaction%>],
+    @chart_data = {
+      labels: ["対応満足度", "売却スピード","売却価格"],
+      datasets: [{
+          label: @company.name,
+          data: [2,3,4],
+          backgroundColor: "rgba(67, 133, 215, 0.5)",  #グラフ背景色
+          borderColor: "rgba(67, 133, 215, 1)",        #グラフボーダー色
+      }]
+    }
   end
 end
