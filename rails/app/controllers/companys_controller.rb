f3372426d293d794d758cb185b3dff9a6486e819
t4jet <53136890+t4jet@users.noreply.github.com>
2023-08-30 02:02:35 +0000

    bundle exec rubocop -a

--- rails/app/controllers/companys_controller.rb
+++ rails/app/controllers/companys_controller.rb
@@ -7,7 +7,7 @@ class CompanysController < ApplicationController
     @office = Office.find(params[:ieul_office_id])
     @ieul_id = Office.find(params[:ieul_office_id]).ieul_id
     @company = Company.find(@ieul_id)
-    @company_name =  @company.name
+    @company_name = @company.name
     @reviews = @office.reviews.order(release_date: :desc).limit(5)
 
     total_satisfaction = @reviews.sum(:contract_price_satisfaction)
@@ -19,14 +19,13 @@ class CompanysController < ApplicationController
     total_sales_speed_satisfaction = @reviews.sum(:sales_speed_satisfaction)
     @average_sales_speed_satisfaction = total_sales_speed_satisfaction / @reviews.length.to_f
 
-    # data: [<%= @average_satisfaction%>, <%= @average_contract_price_satisfaction %>, <%= @average_sales_speed_satisfaction%>],
     @chart_data = {
-      labels: ["対応満足度", "売却スピード","売却価格"],
+      labels: %w[対応満足度 売却スピード 売却価格],
       datasets: [{
-          label: @company.name,
-          data: [ @average_satisfaction,@average_contract_price_satisfaction,@average_sales_speed_satisfaction],
-          backgroundColor: "rgba(67, 133, 215, 0.5)",  #グラフ背景色
-          borderColor: "rgba(67, 133, 215, 1)",        #グラフボーダー色
+        label: @company.name,
+        data: [@average_satisfaction, @average_contract_price_satisfaction, @average_sales_speed_satisfaction],
+        backgroundColor: 'rgba(67, 133, 215, 0.5)', # グラフ背景色
+        borderColor: 'rgba(67, 133, 215, 1)' # グラフボーダー色
       }]
     }
   end
