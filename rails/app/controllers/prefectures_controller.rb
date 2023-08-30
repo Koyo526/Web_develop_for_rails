ef3b8e2467c529af885804ed47a9dee75d1756ff
t4jet <53136890+t4jet@users.noreply.github.com>
2023-08-30 07:08:02 +0000

    コメントアウトを削除

--- rails/app/controllers/prefectures_controller.rb
+++ rails/app/controllers/prefectures_controller.rb
@@ -2,11 +2,10 @@
 
 class PrefecturesController < ApplicationController
   def index
-    @offices = Office.joins(:assessable_cities).where(assessable_cities: { id: params[:city_id] }) # Office.find_by(Office.company_id.assessable_areas: params[:city_id])
+    @offices = Office.joins(:assessable_cities).where(assessable_cities: { id: params[:city_id] })
     Rails.logger.debug params[:city_id]
     Rails.logger.debug @offices
 
-    # https://musical-space-rotary-phone-xpw57g5999qfppw6-3000.app.github.dev/prefectures/1/cities/1
     @city = City.find(params[:city_id])
     @cities_without_target_city = @city.prefecture.cities.reject { |city| city == @city }
   end
