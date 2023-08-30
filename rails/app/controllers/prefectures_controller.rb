0a043c63036affd97ab5e9e0896941c142067107
t4jet <53136890+t4jet@users.noreply.github.com>
2023-08-30 07:07:24 +0000

    bundle exec rubocop -A

--- rails/app/controllers/prefectures_controller.rb
+++ rails/app/controllers/prefectures_controller.rb
@@ -3,8 +3,8 @@
 class PrefecturesController < ApplicationController
   def index
     @offices = Office.joins(:assessable_cities).where(assessable_cities: { id: params[:city_id] }) # Office.find_by(Office.company_id.assessable_areas: params[:city_id])
-    puts params[:city_id]
-    puts @offices
+    Rails.logger.debug params[:city_id]
+    Rails.logger.debug @offices
 
     # https://musical-space-rotary-phone-xpw57g5999qfppw6-3000.app.github.dev/prefectures/1/cities/1
     @city = City.find(params[:city_id])
