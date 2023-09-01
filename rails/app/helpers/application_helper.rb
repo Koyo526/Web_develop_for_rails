1b4ba37bffb477ef181b7ae177394e9657969813
KoMaTop10 <74088338+KoMaTop10@users.noreply.github.com>
2023-09-01 02:33:06 +0000

    rubocopの実行

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-    def number_format(num)
-        number_formatted = num /10000
-        return "#{number_formatted}万円'"
-
-    end
+  def number_format(num)
+    number_formatted = num / 10_000
+    number_formatted.to_s.insert(-1, '万円')
+    "#{number_formatted}万円"
+  end
 end
